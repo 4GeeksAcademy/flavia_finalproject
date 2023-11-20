e1918f082e488455b2c7708a410b5bdbe23fc0a8
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-11-20 20:45:09 +0000

    nuevo botón para cargar más vídeos de ejercicios

--- src/front/js/pages/exercises.js
+++ src/front/js/pages/exercises.js
@@ -52,9 +52,15 @@ export const Exercises = () => {
                 </div>
             )}
             {!store.loading && store.videos && visibleVideos < store.videos.length && (
-                 <div className="load-more-container">
-                 <button className="video-load-more" onClick={handleLoadMore}>Cargar más</button>
-             </div>
+                <div className="load-more-container">
+                    <div className="video-load-more" onClick={handleLoadMore} tabindex="0" class="plusButton">
+                        <svg class="plusIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                            <g mask="url(#mask0_21_345)">
+                                <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z"></path>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
             )}
         </div>
     );
