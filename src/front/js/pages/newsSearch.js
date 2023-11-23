9ec664b6cceb590b8df36ed922e9235a8b3cb558
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-11-23 19:12:59 +0000

    reducción del número de artículos mostrados en la seccion news para no repetir imagenes, y font weight para el titulo de los articulos

--- src/front/js/pages/newsSearch.js
+++ src/front/js/pages/newsSearch.js
@@ -13,6 +13,11 @@ export const NewsSearch = () => {
         actions.fetchArticles(fromDate, toDate, sortBy)
     }
 
+    const imageUrls = [
+        "https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1494390248081-4e521a5940db?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1496412705862-e0088f16f791?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1498837167922-ddd27525d352?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1497888329096-51c27beff665?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1464454709131-ffd692591ee5?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1467453678174-768ec283a940?q=80&w=2044&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1535914254981-b5012eebbd15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1521986329282-0436c1f1e212?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ];
+
     return (
         <div>
             <div className="form-container">
@@ -44,8 +49,8 @@ export const NewsSearch = () => {
                 <button className="search-button" onClick={handle_fetchArticles}>Search</button>
             </div>
             <div className="cards-row">
-                {store.articles.map((article, index) => (
-                    <SearchCard key={article.title} title={article.title} url={article.url} author={article.author} />
+                {store.articles.slice(0, 9).map((article, index) => (
+                    <SearchCard key={article.title} title={article.title} url={article.url} author={article.author} imageUrl={imageUrls[index % imageUrls.length]} />
                 ))}
             </div>
         </div>
