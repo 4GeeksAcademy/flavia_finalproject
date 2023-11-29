fd3bdfa5022c815543b62e7bd961e6dbe9b37e9a
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-11-29 23:41:04 +0000

    eliminé el componente fav food cardd

--- src/front/js/pages/user_fav_foods.js
+++ src/front/js/pages/user_fav_foods.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { FoodCard } from "../component/foodCard";
+import "../../styles/user_fav_foods.css"
 
 export const UserFavFoods = () => {
     const { store, actions } = useContext(Context)
@@ -8,12 +9,18 @@ export const UserFavFoods = () => {
     useEffect(() => {
         actions.userFavFoods(accessToken);
     }, [accessToken]);
+
     return (
         <div className='food-cards-container'>
             <h4 className='my-appointments'>My Favorite Foods</h4>
             {
                 store.user_fav_foods.map((item, index) => (
-                    <FoodCard label={item.foodName} ENERC_KCAL={item.calories} foodId={item.foodId} measureURI={item.measureURI} />
+                    <div className='food-card2'>
+                        <FoodCard label={item.foodName} ENERC_KCAL={item.calories} foodId={item.foodId} measureURI={item.measureURI} />
+                        <button className='deleteFavFood'>
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </div>
                 ))
             }
         </div>
